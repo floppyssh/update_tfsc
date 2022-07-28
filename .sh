1b5ca705ed7e6e4296f31f858715baa4949e0181
floppyssh <32429401+floppyssh@users.noreply.github.com>
2022-07-28 12:37:54 +0600

    Update .sh

--- .sh
+++ .sh
@@ -3,7 +3,7 @@
 pkill -9 tfsc
 
 cd $HOME/tfsc/
-wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_v0.1.1_89ce99f_devnet
+wget -O $HOME/tfsc/tfsc https://fastcdn.uscloudmedia.com/transformers/test/ttfsc_v0.1.2_4d8f89b_devnet
 
 chmod +x $HOME/tfsc/tfsc
 
